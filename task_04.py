4dd2e31f70c04a8080c4b293e5305c31b5a225b9
apag101 <apag101@yahoo.com>
2014-09-14 13:31:50 -0400

    Lesson 3 correction 15

--- task_04.py
+++ task_04.py
@@ -10,7 +10,7 @@ TIME = raw_input('Enter time as a 4 digit number. EG(0600)')
 DAY = raw_input('What day of the week is it ?').lower()
 SNOOZE = True if DAY in WEEKEND or TIME < 0600 else False
 
-if SNOOZE == True:
+if SNOOZE is True:
     print ''
 else:
     print 'Beep! Beep! Beep! Beep! Beep!'
