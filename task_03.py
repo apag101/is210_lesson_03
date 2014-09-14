c7b64ad6507177b806c3c06577bc4d1435e0d837
apag101 <apag101@yahoo.com>
2014-09-14 12:46:05 -0400

    Lesson 3 correction 6

--- task_03.py
+++ task_03.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """LESSON3:
-TASK3 
+TASK3
 Practice nested statements."""
 
-PARENT = ['None', 'Seattle Gray', 'Manatee', 'Ceramic Glaze',
+P = ['None', 'Seattle Gray', 'Manatee', 'Ceramic Glaze',
           'Culumus Nimbus', 'Platinum Mist', 'Spartan Sage']
 
 NONE = ['Seattle Gray', 'Manatee']
@@ -15,38 +15,38 @@ CU = ['Off-White', 'Paper White']
 PL = ['Bone White', 'Just White']
 SP = ['Fractal White', 'Not White']
 
-SELECTION = raw_input('Enter one of the following parent colors {0}'.format(PARENT))
+SEL = raw_input('Enter one of the following parent colors {0}'.format(P))
 
-if SELECTION == 'None':
+if SEL == 'None':
     TYPE = 'Base'
     COLOR = raw_input('Enter one of the following colors {0}'.format(NONE))
     COLOR = COLOR if COLOR in NONE else None
-elif SELECTION == 'Seattle Gray':
+elif SEL == 'Seattle Gray':
     TYPE = 'Accent'
     COLOR = raw_input('Enter one of the following colors {0}'.format(SG))
     COLOR = COLOR if COLOR in SG else None
-elif SELECTION == 'Manatee':
+elif SEL == 'Manatee':
     TYPE = 'Accent'
     COLOR = raw_input('Enter one of the following colors {0}'.format(MT))
     COLOR = COLOR if COLOR in MT else None
-elif SELECTION == 'Ceramic Glaze':
+elif SEL == 'Ceramic Glaze':
     TYPE = 'Highlight'
     COLOR = raw_input('Enter one of the following colors {0}'.format(CR))
     COLOR = COLOR if COLOR in CR else None
-elif SELECTION == 'Culumus Nimbus':
+elif SEL == 'Culumus Nimbus':
     TYPE = 'Highlight'
     COLOR = raw_input('Enter one of the following colors {0}'.format(CU))
-    COLOR = COLOR if COLOR in CU else None  
-elif SELECTION == 'Platinum Mist':
+    COLOR = COLOR if COLOR in CU else None
+elif SEL == 'Platinum Mist':
     TYPE = 'Highlight'
     COLOR = raw_input('Enter one of the following colors {0}'.format(PL))
     COLOR = COLOR if COLOR in PL else None
-elif SELECTION == 'Spartan Sage':
+elif SEL == 'Spartan Sage':
     TYPE = 'Highlight'
     COLOR = raw_input('Enter one of the following colors {0}'.format(SP))
     COLOR = COLOR if COLOR in SP else None
 else:
-    if SELECTION not in PARENT or COLOR is None:
+    if SEL not in P or COLOR is None:
         print 'You have entered an invalid color'
-COMPLETE = '{0}  has a parent color of {1} and a type of {2}'.format(COLOR, SELECTION, TYPE)
-print COMPLETE
+C = '{0} a parent color of {1} and a type of {2}'.format(COLOR, SEL, TYPE)
+print C
