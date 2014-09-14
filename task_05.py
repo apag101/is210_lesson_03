c2976659ab030b1c9ccca30bbda5d4a6d1b54900
apag101 <apag101@yahoo.com>
2014-09-14 13:42:46 -0400

    Lesson 3 correction 16

--- task_05.py
+++ task_05.py
@@ -6,36 +6,36 @@ Compound Examples."""
 
 NAME = raw_input('What is your name ?')
 P = raw_input('What is the principal amount ?')
-t = raw_input('How many years is the loan ?')
+T = raw_input('How many years is the loan ?')
 Q = raw_input('Are you pre-qualified ? (Yes or No)').lower()
-n = 12
-r = 0
+N = 12
+R = 0
 
 if int(P) < 200000:
-    if int(t) < 16:
-        r = .0363 if Q == 'yes' else .0465
-    if int(t) > 15 < 21:
-        r = .0404 if Q == 'yes' else .0498
-    if int(t) > 20 < 31:
-        r = .0577 if Q == 'yes' else .0639
+    if int(T) < 16:
+        R = .0363 if Q == 'yes' else .0465
+    if int(T) > 15 < 21:
+        R = .0404 if Q == 'yes' else .0498
+    if int(T) > 20 < 31:
+        R = .0577 if Q == 'yes' else .0639
 if int(P) > 200000 < 1000000:
-    if int(t) < 16:
-        r = .0302 if Q == 'yes' else .0398
-    if int(t) > 15 < 21:
-        r = .0327 if Q == 'yes' else .0408
-    if int(t) > 20 < 31 and Q == 'yes':
-        r = .0466
+    if int(T) < 16:
+        R = .0302 if Q == 'yes' else .0398
+    if int(T) > 15 < 21:
+        R = .0327 if Q == 'yes' else .0408
+    if int(T) > 20 < 31 and Q == 'yes':
+        R = .0466
 if int(P) > 1000000:
-    if int(t) < 16 and Q == 'yes':
-        r = .0205
-    if int(t) > 15 < 21 and Q == 'yes':
-        r = .0262
+    if int(T) < 16 and Q == 'yes':
+        R = .0205
+    if int(T) > 15 < 21 and Q == 'yes':
+        R = .0262
 
-TOTAL = int(round(int(P) * ( 1 + r / n) ** (n * int(t)),0))
+TOTAL = int(round(int(P) * ( 1 + R / N) ** (N * int(T)),0))
 REPORT = '''Loan Report For: {n}
 -----------------------------
-    Principal:      {p} 
-    Duration:       {T}
+    Principal:      {pr} 
+    Duration:       {Time}
     Prequalified?:  {q}
-    Total:          {t}'''.format(n=NAME,p=P, T=t, q=Q, t=TOTAL)
+    Total:          {total}'''.format(n = NAME, pr = P, Time = T, q = Q, total = TOTAL)
 print REPORT
