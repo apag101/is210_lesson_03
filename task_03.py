e70bc185d311d4c2d1c9bf9e9f69590051720e8b
apag101 <apag101@yahoo.com>
2014-09-14 12:59:45 -0400

    Lesson 3 correction 8

--- task_03.py
+++ task_03.py
@@ -4,7 +4,8 @@
 TASK3
 Practice nested statements."""
 
-P = ['None', 'Seattle Gray', 'Manatee', 'Ceramic Glaze', 'Culumus Nimbus', 'Platinum Mist', 'Spartan Sage']
+P = ['None', 'Seattle Gray', 'Manatee', 'Ceramic Glaze',
+'Culumus Nimbus', 'Platinum Mist', 'Spartan Sage']
 
 NONE = ['Seattle Gray', 'Manatee']
 SG = ['Ceramic Glaze', 'Culumus Nimbus']
@@ -19,31 +20,31 @@ SEL = raw_input('Enter one of the following parent colors {0}'.format(P))
 if SEL == 'None':
     TYPE = 'Base'
     COLOR = raw_input('Enter one of the following colors {0}'.format(NONE))
-    COLOR = COLOR if COLOR in NONE else None
+    COLOR = COLOR if COLOR in NONE else 'None'
 elif SEL == 'Seattle Gray':
     TYPE = 'Accent'
     COLOR = raw_input('Enter one of the following colors {0}'.format(SG))
-    COLOR = COLOR if COLOR in SG else None
+    COLOR = COLOR if COLOR in SG else 'None'
 elif SEL == 'Manatee':
     TYPE = 'Accent'
     COLOR = raw_input('Enter one of the following colors {0}'.format(MT))
-    COLOR = COLOR if COLOR in MT else None
+    COLOR = COLOR if COLOR in MT else 'None'
 elif SEL == 'Ceramic Glaze':
     TYPE = 'Highlight'
     COLOR = raw_input('Enter one of the following colors {0}'.format(CR))
-    COLOR = COLOR if COLOR in CR else None
+    COLOR = COLOR if COLOR in CR else 'None'
 elif SEL == 'Culumus Nimbus':
     TYPE = 'Highlight'
     COLOR = raw_input('Enter one of the following colors {0}'.format(CU))
-    COLOR = COLOR if COLOR in CU else None
+    COLOR = COLOR if COLOR in CU else 'None'
 elif SEL == 'Platinum Mist':
     TYPE = 'Highlight'
     COLOR = raw_input('Enter one of the following colors {0}'.format(PL))
-    COLOR = COLOR if COLOR in PL else None
+    COLOR = COLOR if COLOR in PL else 'None'
 elif SEL == 'Spartan Sage':
     TYPE = 'Highlight'
     COLOR = raw_input('Enter one of the following colors {0}'.format(SP))
-    COLOR = COLOR if COLOR in SP else None
+    COLOR = COLOR if COLOR in SP else 'None'
 else:
     if SEL not in P or COLOR is None:
         print 'You have entered an invalid color'
