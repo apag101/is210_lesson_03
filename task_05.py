9940828ccfd7682c5c44503b64153c66c68dd60b
apag101 <apag101@yahoo.com>
2014-09-15 09:32:02 -0400

    Lesson 3 correction 32

--- task_05.py
+++ task_05.py
@@ -13,23 +13,23 @@ R = 0
 TOTAL = None
 
 if int(P) > 0 <= 199999:
-    if int(T) >= 1 <= 15:
+    if int(T) >= 1 and int(T) <= 15:
         R = .0363 if Q == 'y' else .0465
-    if int(T) >= 16 <= 20:
+    if int(T) >= 16 and int(T) <= 20:
         R = .0404 if Q == 'y' else .0498
-    if int(T) >= 21 <= 30:
+    if int(T) >= 21 and int(T) <= 30:
         R = .0577 if Q == 'y' else .0639
 if int(P) >= 200000 <= 999999:
-    if int(T) >= 1 <= 15:
+    if int(T) >= 1 and int(T) <= 15:
         R = .0302 if Q == 'y' else .0398
-    if int(T) >= 16 <= 20:
+    if int(T) >= 16 and int(T) <= 20:
         R = .0327 if Q == 'y' else .0408
-    if int(T) >= 21 <= 30 and Q == 'y':
+    if int(T) >= 21 and int(T) <= 30 and Q == 'y':
         R = .0466
 if int(P) >= 1000000:
     if int(T) <= 15 and Q == 'y':
         R = .0205
-    if int(T) >= 16 <= 20 and Q == 'y':
+    if int(T) >= 16 and int(T) <= 20 and Q == 'y':
         R = .0262
 
 TOTAL = int(round(int(P) * (1 + decimal.Decimal(R) / N) ** (N * int(T)), 0))
