2d275096e9be8b92f1caa3a7220beae60f254fd3
apag101 <apag101@yahoo.com>
2014-09-15 00:04:25 -0400

    Lesson 3 correction 25

--- task_05.py
+++ task_05.py
@@ -3,7 +3,7 @@
 """LESSON3:
 TASK5
 Compound Examples."""
-
+import decimal
 NAME = raw_input('What is your name ?')
 P = raw_input('What is the principal amount ?')
 T = raw_input('How many years is the loan ?')
@@ -13,25 +13,28 @@ R = 0
 
 if int(P) < 200000:
     if int(T) < 16:
-        R = .0363 if Q == 'yes' else .0465
+        R = .0363 if Q == 'yes' or Q == 'y' else .0465
     if int(T) > 15 < 21:
-        R = .0404 if Q == 'yes' else .0498
+        R = .0404 if Q == 'yes' or Q == 'y' else .0498
     if int(T) > 20 < 31:
-        R = .0577 if Q == 'yes' else .0639
+        R = .0577 if Q == 'yes' or Q == 'y' else .0639
 if int(P) > 200000 < 1000000:
     if int(T) < 16:
-        R = .0302 if Q == 'yes' else .0398
+        R = .0302 if Q == 'yes' or Q == 'y' else .0398
     if int(T) > 15 < 21:
-        R = .0327 if Q == 'yes' else .0408
-    if int(T) > 20 < 31 and Q == 'yes':
+        R = .0327 if Q == 'yes' or Q == 'y' else .0408
+    if int(T) > 20 < 31 and Q == 'yes' or Q == 'y':
         R = .0466
 if int(P) > 1000000:
-    if int(T) < 16 and Q == 'yes':
+    if int(T) < 16 and Q == 'yes' or Q == 'y':
         R = .0205
-    if int(T) > 15 < 21 and Q == 'yes':
+    if int(T) > 15 < 21 and Q == 'yes' or Q == 'y':
         R = .0262
+else:
+    TOTAL = None
 
-TOTAL = int(round(int(P) * (1 + R / N) ** (N * int(T)), 0))
+TOTAL = int(round(int(P) * (1 + decimal.Decimal(R) / N) ** (N * int(T)), 0))
+
 REPORT = '''Loan Report For: {n}
 -----------------------------
     Principal:      {pr} 
