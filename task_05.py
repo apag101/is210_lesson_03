4def9b957c5e7cfaba66e5ea0be8b9b8c59476df
apag101 <apag101@yahoo.com>
2014-09-15 09:58:56 -0400

    Lesson 3 correction 32

--- task_05.py
+++ task_05.py
@@ -5,34 +5,34 @@ TASK5
 Compound Examples."""
 import decimal
 NAME = raw_input('What is your name ?')
-P = raw_input('What is the principal amount ?')
-T = raw_input('How many years is the loan ?')
+P = int(raw_input('What is the principal amount ?'))
+T = int(raw_input('How many years is the loan ?'))
 Q = raw_input('Are you pre-qualified ? (Yes or No)').lower()
 N = 12
 R = 0
 TOTAL = None
 
-if int(P) > 0 <= 199999:
-    if int(T) >= 1 and int(T) <= 15:
+if P > 0 and P <= 199999:
+    if T >= 1 and T <= 15:
         R = .0363 if Q == 'y' else .0465
-    if int(T) >= 16 and int(T) <= 20:
+    if T >= 16 and T <= 20:
         R = .0404 if Q == 'y' else .0498
-    if int(T) >= 21 and int(T) <= 30:
+    if T >= 21 and T <= 30:
         R = .0577 if Q == 'y' else .0639
-if int(P) >= 200000 <= 999999:
-    if int(T) >= 1 and int(T) <= 15:
+if P >= 200000 and P <= 999999:
+    if T >= 1 and T <= 15:
         R = .0302 if Q == 'y' else .0398
-    if int(T) >= 16 and int(T) <= 20:
+    if T >= 16 and T <= 20:
         R = .0327 if Q == 'y' else .0408
-    if int(T) >= 21 and int(T) <= 30 and Q == 'y':
+    if T >= 21 and T <= 30 and Q == 'y':
         R = .0466
-if int(P) >= 1000000:
-    if int(T) <= 15 and Q == 'y':
+if P >= 1000000:
+    if T <= 15 and Q == 'y':
         R = .0205
-    if int(T) >= 16 and int(T) <= 20 and Q == 'y':
+    if T >= 16 and T <= 20 and Q == 'y':
         R = .0262
 
-TOTAL = int(round(int(P) * (1 + decimal.Decimal(R) / N) ** (N * int(T)), 0))
+TOTAL = int(round(P * (1 + decimal.Decimal(str(R)) / N) ** (N * T), 0))
 
 REPORT = '''Loan Report For: {n}
 -----------------------------
