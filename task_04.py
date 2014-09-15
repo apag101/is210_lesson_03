cc687fa049076812cc064393d5ffd1f2905d414d
apag101 <apag101@yahoo.com>
2014-09-14 23:39:57 -0400

    Lesson 3 correction 24

--- task_04.py
+++ task_04.py
@@ -7,9 +7,10 @@ Ternary Expressions."""
 
 WEEKEND = ['saturday', 'sunday']
 DAY = raw_input('What day of the week is it ?').lower()
-TIME = raw_input('Enter time as a 4 digit number. EG(0600)')
+TIME = int(raw_input('Enter time as a 4 digit number. EG(0600)'))
 
-SNOOZE = True if DAY[0:3] in WEEKEND[0] or DAY[0:3] in WEEKEND[1] or TIME < 0600 else False
+SNOOZE = True if DAY[0:3] in WEEKEND[0] or DAY[0:3] in WEEKEND[1] \
+         or TIME < 600 else False
 
 if SNOOZE is not True:
     print 'Beep! Beep! Beep! Beep! Beep!'
